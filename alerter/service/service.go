70fa3ad6fa1e6ae05cf604c03ccba8bd435f05a4
Jason Wilder <jwilder@microsoft.com>
2023-02-03 12:21:14 -0700

    Replace cobra with urfave for CLI

--- alerter/service/service.go
+++ alerter/service/service.go
@@ -13,7 +13,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/spf13/viper"
 	"go.goms.io/aks/azauth"
 	"go.goms.io/aks/imds"
 	"net/http"
@@ -24,11 +23,24 @@ import (
 	"github.com/Azure/adx-mon/alerter/rules"
 )
 
+type AlerterOpts struct {
+	Dev            bool
+	KustoEndpoints map[string]string
+	Region         string
+	AlertAddr      string
+	Cloud          string
+	Port           int
+	Concurrency    int
+	MSIID          string
+	MSIResource    string
+}
+
 type Alerter struct {
 	log      logger.Logger
 	clients  map[string]*kusto.Client
 	queue    chan struct{}
 	alertCli *alert.Client
+	opts     AlerterOpts
 }
 
 var ruleErrorCounter = promauto.NewCounterVec(
@@ -41,21 +53,21 @@ var ruleErrorCounter = promauto.NewCounterVec(
 	[]string{"region"},
 )
 
-func New() (*Alerter, error) {
+func New(opts AlerterOpts) (*Alerter, error) {
 	log, err := newLogger()
 	if err != nil {
 		return nil, fmt.Errorf("failed to construct logger: %w", err)
 	}
 
-	if err := rules.VerifyRules(viper.GetString("region")); err != nil {
+	if err := rules.VerifyRules(opts.Region); err != nil {
 		return nil, err
 	}
 
 	var auth kusto.Authorization
-	if viper.GetBool("dev") {
-		auth, err = devAuth()
+	if opts.Dev {
+		auth, err = devAuth(opts)
 	} else {
-		auth, err = prodAuth()
+		auth, err = prodAuth(opts)
 	}
 	if err != nil {
 		return nil, fmt.Errorf("failed to create authorizer: %w", err)
@@ -64,25 +76,14 @@ func New() (*Alerter, error) {
 	l2m := &Alerter{
 		log:     log,
 		clients: make(map[string]*kusto.Client),
-		queue:   make(chan struct{}, viper.GetInt("concurrency")),
+		queue:   make(chan struct{}, opts.Concurrency),
 	}
-	infraEndpoint := viper.GetString("kusto-infra-endpoint")
-	l2m.clients["AKSinfra"], err = kusto.New(infraEndpoint, auth)
-	if err != nil {
-		return nil, fmt.Errorf("kusto infra client=%s: %w", infraEndpoint, err)
-	}
-	serviceEndpoint := viper.GetString("kusto-service-endpoint")
-	l2m.clients["AKSprod"], err = kusto.New(serviceEndpoint, auth)
-	if err != nil {
-		return nil, fmt.Errorf("kusto service client=%s: %w", serviceEndpoint, err)
-	}
-	customerEndpoint := viper.GetString("kusto-customer-endpoint")
-	l2m.clients["AKSccplogs"], err = kusto.New(customerEndpoint, auth)
-	if err != nil {
-		return nil, fmt.Errorf("kusto customer client=%s: %w", customerEndpoint, err)
-	}
-	if viper.GetString("region") == "" {
-		return nil, errors.New("missing or invalid region")
+
+	for name, endpoint := range opts.KustoEndpoints {
+		l2m.clients[name], err = kusto.New(endpoint, auth)
+		if err != nil {
+			return nil, fmt.Errorf("kusto client=%s: %w", endpoint, err)
+		}
 	}
 
 	alertCli, err := alert.NewClient(time.Minute)
@@ -96,7 +97,7 @@ func New() (*Alerter, error) {
 
 func (l *Alerter) Run() error {
 	executor := &engine.Executor{
-		AlertAddr: viper.GetString("alert-address"),
+		AlertAddr: l.opts.AlertAddr,
 		AlertCli:  l.alertCli,
 	}
 
@@ -107,9 +108,9 @@ func (l *Alerter) Run() error {
 	logger.Info("Starting log-to-metrics")
 
 	go func() {
-		logger.Info("Listening at :%d", viper.GetInt("port"))
+		logger.Info("Listening at :%d", l.opts.Port)
 		http.Handle("/metrics", promhttp.Handler())
-		if err := http.ListenAndServe(fmt.Sprintf(":%d", viper.GetInt("port")), nil); err != nil {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", l.opts.Port), nil); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
@@ -118,15 +119,15 @@ func (l *Alerter) Run() error {
 	return executor.Execute(context.Background(), l, log)
 }
 
-func prodAuth() (kusto.Authorization, error) {
-	msiID := viper.GetString("msi-id")
-	msiResource := viper.GetString("msi-resource")
+func prodAuth(opts AlerterOpts) (kusto.Authorization, error) {
+	msiID := opts.MSIID
+	msiResource := opts.MSIResource
 	if msiID == "" && msiResource == "" {
 		return kusto.Authorization{}, errors.New("missing required parameter for MSI")
 	}
 
 	if msiResource != "" {
-		cloud := viper.GetString("cloud")
+		cloud := opts.Cloud
 		if strings.ToLower(cloud) == "azurecloud" {
 			cloud = "AzurePublicCloud"
 		}
@@ -170,7 +171,7 @@ func (l *Alerter) Query(ctx context.Context, r rules.Rule, fn func(string, rules
 	})
 }
 
-func devAuth() (kusto.Authorization, error) {
+func devAuth(opts AlerterOpts) (kusto.Authorization, error) {
 	// To run queries locally in dev on your laptop, you'll need to create an access token for the Kusto
 	// cluster you want to query via the AZ cli.  This uses your corp account to get an
 	// access token to Kusto and then uses the token to auth to Kusto for you.
@@ -187,7 +188,7 @@ func devAuth() (kusto.Authorization, error) {
 	// 		--kusto-customer-endpoint https://aksccplogs.centralus.kusto.windows.net \
 	// 		--region westeurope \
 	//		--rule NameOfRuleToTest
-	auth, err := auth.NewAuthorizerFromCLIWithResource(viper.GetString("kusto-infra-endpoint"))
+	auth, err := auth.NewAuthorizerFromCLIWithResource(opts.KustoEndpoints["AKSinfra"])
 	if err != nil {
 		return kusto.Authorization{}, fmt.Errorf("failed to created authorized: %w", err)
 	}
--- cmd/alerter/main.go
+++ cmd/alerter/main.go
@@ -1,48 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/Azure/adx-mon/alerter/service"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"github.com/Azure/adx-mon/logger"
+	"github.com/urfave/cli/v2" // imports as package "cli"
+	"os"
 )
 
 func main() {
-	cmd := &cobra.Command{
-		Use:   "l2m",
-		Short: "Emits metrics based on Kusto query results",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			l2m, err := service.New()
-			if err != nil {
-				return err
-			}
-			return l2m.Run()
+	app := &cli.App{
+		Name:  "alerter",
+		Usage: "adx-mon alerting engine for ADX",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "dev", Usage: "Run on a local dev machine without executing real queries"},
+			&cli.IntFlag{Name: "port", Value: 4023, Usage: "Metrics port number"},
+			// Either the msi-id or msi-resource must be specified
+			&cli.StringFlag{Name: "msi-id", Usage: "MSI client ID"},
+			&cli.StringFlag{Name: "msi-resource", Usage: "MSI resource ID"},
+			&cli.StringFlag{Name: "cloud", Usage: "Azure cloud"},
+			&cli.StringFlag{Name: "region", Usage: "Current region"},
+			&cli.StringFlag{Name: "kusto-service-endpoint", Usage: "Kusto service endpoint"},
+			&cli.StringFlag{Name: "kusto-infra-endpoint", Usage: "Kusto infra endpoint"},
+			&cli.StringFlag{Name: "kusto-customer-endpoint", Usage: "Kusto infra endpoint"},
+			&cli.StringFlag{Name: "alerter-address", Usage: "Address of the alert notification service"},
+			&cli.IntFlag{Name: "concurrency", Value: 10, Usage: "Number of concurrent queries to run"},
+		},
+
+		Action: func(ctx *cli.Context) error {
+			return realMain(ctx)
 		},
 	}
 
-	viper.SetEnvPrefix("L2M")
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-
-	flags := cmd.Flags()
-	flags.Bool("dev", false, "Run on a local dev machine without executing real queries")
-	flags.Int("port", 4023, "Metrics port number")
-	// Either the msi-id or msi-resource must be specified
-	flags.String("msi-id", "", "MSI client ID")
-	flags.String("msi-resource", "", "MSI resource ID")
-	flags.String("cloud", "", "Azure cloud")
-	flags.String("kusto-service-endpoint", "", "Kusto service endpoint")
-	flags.String("kusto-infra-endpoint", "", "Kusto infra endpoint")
-	flags.String("region", "", "Current region")
-	flags.String("underlay", "", "Current underlay")
-	flags.String("alert-address", "", "Address of the alert notification service")
-	viper.BindPFlags(flags)
-	viper.AutomaticEnv()
-
-	if err := cmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if err := app.Run(os.Args); err != nil {
+		logger.Fatal(err.Error())
 	}
+}
+
+func realMain(ctx *cli.Context) error {
+	opts := service.AlerterOpts{
+		Dev:  ctx.Bool("dev"),
+		Port: ctx.Int("port"),
+		KustoEndpoints: map[string]string{
+			"AKSinfra":   ctx.String("kusto-infra-endpoint"),
+			"AKSprod":    ctx.String("kusto-service-endpoint"),
+			"AKSccplogs": ctx.String("kusto-customer-endpoint"),
+		},
+		Region:      ctx.String("region"),
+		Cloud:       ctx.String("cloud"),
+		AlertAddr:   ctx.String("alerter-address"),
+		Concurrency: ctx.Int("concurrency"),
+		MSIID:       ctx.String("msi-id"),
+		MSIResource: ctx.String("msi-resource"),
+	}
+
+	svc, err := service.New(opts)
+	if err != nil {
+		return err
+	}
+	return svc.Run()
 }
