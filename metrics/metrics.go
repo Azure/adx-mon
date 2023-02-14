6e4ce22e40138210a1564265c93494ad73c7ba19
Jason Wilder <jawilder@microsoft.com>
2023-02-14 13:42:28 -0700

    Add query/alert success counters

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -27,8 +27,15 @@ var (
 	AlertQueryFailures = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: Namespace,
 		Subsystem: "alerter",
-		Name:      "alert_query_failures_total",
+		Name:      "query_failures_total",
 		Help:      "Number of alert query failures",
+	}, []string{"namespace", "name"})
+
+	AlertQuerySuccess = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: "alerter",
+		Name:      "query_success_total",
+		Help:      "Number of alert query successfully executed",
 	}, []string{"namespace", "name"})
 
 	NotificationFailures = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -36,5 +43,12 @@ var (
 		Subsystem: "alerter",
 		Name:      "notification_failures_total",
 		Help:      "Number of alert notification failures",
+	}, []string{"namespace", "name"})
+
+	NotificationSuccess = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: "alerter",
+		Name:      "notification_success_total",
+		Help:      "Number of alert notification successfully created",
 	}, []string{"namespace", "name"})
 )
