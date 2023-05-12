f963fcec8e8697cd927391a6bc7ea1d2232c7f61
Mike Keesey <mikekeesey@microsoft.com>
2023-05-11 20:26:01 -0600

    Do not panic on invalid file path
    If users provide an invalid path for rule files, return the error
    provided instead of panic.

--- alerter/rules/localStore_test.go
+++ alerter/rules/localStore_test.go
@@ -1,7 +1,8 @@
 package rules
 
 import (
-	"strings"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -28,10 +29,41 @@ spec:
   destination: "peoplewhocare"
 `
 
-func TestFromPAth(t *testing.T) {
-	s := &fileStore{}
-	err := s.fromStream(strings.NewReader(alertruleExample), "newmexico")
+func TestFromPath(t *testing.T) {
+	testdir := t.TempDir()
+	testfile := filepath.Join(testdir, "test.yaml")
+	err := ioutil.WriteFile(testfile, []byte(alertruleExample), 0644)
 	require.NoError(t, err)
-	require.Equal(t, "foo", s.rules[0].Namespace)
-	require.Equal(t, "bar", s.rules[0].Name)
+
+	type testcase struct {
+		name      string
+		path      string
+		expectErr bool
+	}
+
+	testcases := []testcase{
+		{
+			name:      "valid directory",
+			path:      testdir,
+			expectErr: false,
+		},
+		{
+			name:      "invalid directory",
+			path:      "/tmp/doesnotexist",
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			store, err := FromPath(tc.path, "eastus")
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, "foo", store.rules[0].Namespace)
+				require.Equal(t, "bar", store.rules[0].Name)
+			}
+		})
+	}
 }
