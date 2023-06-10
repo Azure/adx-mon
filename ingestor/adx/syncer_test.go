e5668cf51543dc37e9ad3ef05d6fcac83de56fe6
Jason Wilder <jawilder@microsoft.com>
2023-06-09 22:02:55 -0700

    Add tests for Syncer

---
+++ ingestor/adx/syncer_test.go
@@ -0,0 +1,28 @@
+package adx
+
+import (
+	"testing"
+
+	"github.com/Azure/adx-mon/ingestor/storage"
+	"github.com/Azure/azure-kusto-go/kusto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSyncer_EnsureMapping(t *testing.T) {
+	kcli := kusto.NewMockClient()
+
+	s := NewSyncer(kcli, "db", storage.SchemaMapping{})
+	name, err := s.EnsureMapping("Test")
+	require.NoError(t, err)
+	require.Equal(t, "Test_15745692345339290292", name)
+}
+
+func TestSyncer_EnsureTable(t *testing.T) {
+	kcli := &fakeKustoMgmt{
+		expectedQuery: ".create-merge table ['Test'] ()",
+	}
+
+	s := NewSyncer(kcli, "db", storage.SchemaMapping{})
+	require.NoError(t, s.EnsureTable("Test"))
+	kcli.Verify(t)
+}
