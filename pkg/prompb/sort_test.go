231f2721af387330611a3670e4380ac5335f8065
Jason Wilder <jawilder@microsoft.com>
2023-08-16 23:18:04 -0600

    Fix cardinality counts
    Labels are sorted but the assumption that __name__ is first was
    not valid at times causing invalid metric names to be counted.

---
+++ pkg/prompb/sort_test.go
@@ -0,0 +1,37 @@
+package prompb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSortedLables(t *testing.T) {
+	l := []Label{
+		{
+			Name:  []byte("foo"),
+			Value: []byte("bar"),
+		},
+		{
+			Name:  []byte("__name__"),
+			Value: []byte("name"),
+		},
+		{
+			Name:  []byte("__abc__"),
+			Value: []byte("abc"),
+		},
+
+		{
+			Name:  []byte("Capital"),
+			Value: []byte("capital"),
+		},
+	}
+
+	require.False(t, IsSorted(l))
+	Sort(l)
+	require.True(t, IsSorted(l))
+	require.Equal(t, []byte("__name__"), l[0].Name)
+	require.Equal(t, []byte("__abc__"), l[1].Name)
+	require.Equal(t, []byte("Capital"), l[2].Name)
+	require.Equal(t, []byte("foo"), l[3].Name)
+}
