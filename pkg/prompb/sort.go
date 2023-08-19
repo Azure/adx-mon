6815f4526e143705e5e3fc7cdd0e4741e020b0f9
Jason Wilder <jawilder@microsoft.com>
2023-08-19 16:22:52 -0600

    Use non-allocating compare function for label sorting

--- pkg/prompb/sort.go
+++ pkg/prompb/sort.go
@@ -3,6 +3,8 @@ package prompb
 import (
 	"bytes"
 	"sort"
+	"unicode"
+	"unicode/utf8"
 )
 
 // IsSorted return true if the labels are sorted according to Sort.
@@ -25,5 +27,33 @@ func labelCompare(a, b []byte) bool {
 	} else if bytes.Equal(b, []byte("__name__")) {
 		return false
 	}
-	return bytes.Compare(bytes.ToLower(a), bytes.ToLower(b)) < 0
+	return compareLower(a, b) < 0
+}
+
+func compareLower(sa, sb []byte) int {
+	for {
+		rb, nb := utf8.DecodeRune(sb)
+		ra, na := utf8.DecodeRune(sa)
+
+		if na == 0 && nb > 0 {
+			return -1
+		} else if na > 0 && nb == 0 {
+			return 1
+		} else if na == 0 && nb == 0 {
+			return 0
+		}
+
+		rb = unicode.ToLower(rb)
+		ra = unicode.ToLower(ra)
+
+		if ra < rb {
+			return -1
+		} else if ra > rb {
+			return 1
+		}
+
+		// Trim rune from the beginning of each string.
+		sa = sa[na:]
+		sb = sb[nb:]
+	}
 }
