7cdb41bb11c73e6ec34927a943f643169a1bc4c5
Jesse Thompson <Jesse.Thompson@microsoft.com>
2023-08-24 08:14:17 -0600

    Add batcher testcase that was deleted from previous merge

--- ingestor/cluster/batcher_test.go
+++ ingestor/cluster/batcher_test.go
@@ -92,6 +92,92 @@ func TestBatcher_NewestFirst(t *testing.T) {
 	require.Equal(t, []string{filepath.Join(dir, wal.Filename("db", "Disk", "2359cd7e3aef0001"))}, owner[1])
 }
 
+func TestBatcher_BigFileBatch(t *testing.T) {
+	dir := t.TempDir()
+
+	idgen, err := flake.New()
+	require.NoError(t, err)
+	now := idgen.NextId()
+
+	f, err := os.Create(filepath.Join(dir, wal.Filename("db", "Cpu", now.String())))
+	f.Truncate(100 * 1024) // Meets min transfer size, separate batch
+	require.NoError(t, err)
+	defer f.Close()
+
+	now = idgen.NextId()
+	f1, err := os.Create(filepath.Join(dir, wal.Filename("db", "Cpu", now.String())))
+	require.NoError(t, err)
+	defer f1.Close()
+
+	// This segment is older, but lexicographically greater.  It should be the in the first batch.
+	f2, err := os.Create(filepath.Join(dir, wal.Filename("db", "Disk", "2359cd7e3aef0001")))
+	require.NoError(t, err)
+	defer f2.Close()
+
+	a := &batcher{
+		hostname:        "node1",
+		storageDir:      dir,
+		maxTransferSize: 100 * 1024,
+		maxTransferAge:  time.Minute,
+		Partitioner:     &fakePartitioner{owner: "node1"},
+		Segmenter:       &fakeSegmenter{active: wal.Filename("db", "Memory", "aaaa")},
+	}
+	owned, notOwned, err := a.processSegments()
+	require.NoError(t, err)
+	require.Equal(t, 3, len(owned))
+	require.Equal(t, 0, len(notOwned))
+	require.Equal(t, []string{f1.Name()}, owned[0])
+	require.Equal(t, []string{f.Name()}, owned[1])
+	require.Equal(t, []string{filepath.Join(dir, wal.Filename("db", "Disk", "2359cd7e3aef0001"))}, owned[2])
+}
+
+func TestBatcher_BigBatch(t *testing.T) {
+	dir := t.TempDir()
+
+	idgen, err := flake.New()
+	require.NoError(t, err)
+	now := idgen.NextId()
+
+	f, err := os.Create(filepath.Join(dir, wal.Filename("db", "Cpu", now.String())))
+	f.Truncate(50 * 1024)
+	require.NoError(t, err)
+	defer f.Close()
+
+	now = idgen.NextId()
+	f1, err := os.Create(filepath.Join(dir, wal.Filename("db", "Cpu", now.String())))
+	f1.Truncate(50 * 1024) // Current batch size meets min transfer size, this should trigger a new batch
+	require.NoError(t, err)
+	defer f1.Close()
+
+	now = idgen.NextId()
+	f2, err := os.Create(filepath.Join(dir, wal.Filename("db", "Cpu", now.String())))
+	require.NoError(t, err)
+	defer f2.Close()
+
+	// This segment is older, but lexicographically greater.  It should be the in the first batch.
+	f4, err := os.Create(filepath.Join(dir, wal.Filename("db", "Disk", "2359cd7e3aef0001")))
+	require.NoError(t, err)
+	defer f4.Close()
+
+	a := &batcher{
+		hostname:        "node1",
+		storageDir:      dir,
+		maxTransferSize: 100 * 1024,
+		maxTransferAge:  time.Minute,
+		Partitioner:     &fakePartitioner{owner: "node1"},
+		Segmenter:       &fakeSegmenter{active: wal.Filename("db", "Memory", "aaaa")},
+	}
+	owned, notOwned, err := a.processSegments()
+
+	require.NoError(t, err)
+	require.Equal(t, 3, len(owned))
+	require.Equal(t, 0, len(notOwned))
+
+	require.Equal(t, []string{f2.Name()}, owned[0])
+	require.Equal(t, []string{f.Name(), f1.Name()}, owned[1])
+	require.Equal(t, []string{filepath.Join(dir, wal.Filename("db", "Disk", "2359cd7e3aef0001"))}, owned[2])
+}
+
 type fakePartitioner struct {
 	owner string
 }
