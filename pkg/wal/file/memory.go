9a9c6eec9fa7587039837b2778788fd3230337da
Jesse Thompson <Jesse.Thompson@microsoft.com>
2023-10-06 12:47:49 -0600

    Make the in-memory WAL store safe for concurrent reads

--- pkg/wal/file/memory.go
+++ pkg/wal/file/memory.go
@@ -16,15 +16,26 @@ var (
 	// cases; however, we're only utilizing this path for Collector wrt logs,
 	// and Collector _is_ handling deletions through the file.Remove method.
 	mufs sync.RWMutex
-	mfs  map[string]*Memory
+	// mfs is our in-memory filesystem. We map filenames to MemF instances.
+	mfs map[string]*MemF
 )
 
+// MemF represents a file on disk and contains the actual bytes, FileMode and mutex.
+type MemF struct {
+	sync.RWMutex
+	b    []byte
+	mode *Info
+}
+
+// Memory implements our File interface and is an instance of metadata for an MemF,
+// which allows us to support concurrent readers, where Memory contains instance
+// specific offsets and state onto the actual byte array backing as repsented by MemF.
 type Memory struct {
 	sync.RWMutex
-	b      []byte
-	mode   *Info
 	offset int64
 	closed bool
+	name   string
+	perm   os.FileMode
 }
 
 type Info struct {
@@ -60,11 +71,12 @@ func (i Info) Sys() any {
 }
 
 func (m *Memory) Create(name string) (File, error) {
+	// Lock our filesystem
 	mufs.Lock()
 	defer mufs.Unlock()
 
 	if mfs == nil {
-		mfs = make(map[string]*Memory)
+		mfs = make(map[string]*MemF)
 	}
 
 	// If the instance already exists, it's truncated
@@ -73,17 +85,18 @@ func (m *Memory) Create(name string) (File, error) {
 	// First we'll check our store
 	f, ok := mfs[name]
 	if ok {
+		// Truncate the file
 		f.Lock()
-		f.b = nil
+		f.b = f.b[:0]
 		f.mode.size = 0
 		f.mode.modtime = time.Now()
 		f.Unlock()
 
-		return f, nil
+		return &Memory{name: name}, nil
 	}
 
 	// Create a new instance and store it
-	f = &Memory{
+	f = &MemF{
 		mode: &Info{
 			name:    name,
 			modtime: time.Now(),
@@ -92,15 +105,19 @@ func (m *Memory) Create(name string) (File, error) {
 	}
 	mfs[name] = f
 
-	return f, nil
+	return &Memory{
+		name: name,
+		perm: os.FileMode(os.O_RDWR),
+	}, nil
 }
 
 func (m *Memory) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
+	// Lock our filesystem
 	mufs.Lock()
 	defer mufs.Unlock()
 
 	if mfs == nil {
-		mfs = make(map[string]*Memory)
+		mfs = make(map[string]*MemF)
 	}
 
 	// Retrieve the instance from our store
@@ -108,7 +125,7 @@ func (m *Memory) OpenFile(name string, flag int, perm os.FileMode) (File, error)
 
 	// No instance exists but our flag specifies to create a new instance
 	if !ok && flag&os.O_CREATE == 0 {
-		f = &Memory{
+		f = &MemF{
 			mode: &Info{
 				name:    name,
 				perm:    perm,
@@ -117,80 +134,107 @@ func (m *Memory) OpenFile(name string, flag int, perm os.FileMode) (File, error)
 		}
 
 		mfs[name] = f
-		return f, nil
+		return &Memory{name: name}, nil
 	}
 
 	// If the instance exits, it's opened with O_RDONLY
 	// https://pkg.go.dev/os#OpenFile
 	if ok {
-		f.Lock()
-		f.closed = false
-		f.offset = 0
-		f.mode.perm = fs.FileMode(os.O_RDONLY)
-		f.Unlock()
 
-		mfs[name] = f
-		return f, nil
+		return &Memory{
+			name: f.mode.name,
+			perm: fs.FileMode(os.O_RDONLY),
+		}, nil
 	}
 
 	return nil, fs.ErrNotExist
 }
 
 func (m *Memory) Open(name string) (File, error) {
-	mufs.Lock()
-	defer mufs.Unlock()
+	// Lock our filesystem
+	mufs.RLock()
+	defer mufs.RUnlock()
 
 	if mfs == nil {
-		mfs = make(map[string]*Memory)
+		return nil, fs.ErrNotExist
 	}
 
 	// If the instance exits, it's opened with O_RDONLY
 	// https://pkg.go.dev/os#Open
 	f, ok := mfs[name]
 	if ok {
-		f.Lock()
-		f.closed = false
-		f.offset = 0
-		f.mode.perm = fs.FileMode(os.O_RDONLY)
-		f.Unlock()
-
-		mfs[name] = f
-		return f, nil
+		return &Memory{
+			name: f.mode.name,
+			perm: fs.FileMode(os.O_RDONLY),
+		}, nil
 	}
 
 	return nil, fs.ErrNotExist
 }
 
 func (m *Memory) Remove(name string) error {
+	// Lock our filesystem
 	mufs.Lock()
 	defer mufs.Unlock()
 
 	if mfs == nil {
-		mfs = make(map[string]*Memory)
+		mfs = make(map[string]*MemF)
 		return nil
 	}
 
+	// Remove the instance from our store
 	delete(mfs, name)
 
 	return nil
 }
 
 func (m *Memory) Stat() (fs.FileInfo, error) {
+	// Lock our instance
 	m.RLock()
 	defer m.RUnlock()
 
-	return m.mode, nil
+	// Lock our filesystem
+	mufs.RLock()
+	defer mufs.RUnlock()
+
+	// If our filesystem is nil, we don't have any instances
+	if mfs == nil {
+		return nil, fs.ErrNotExist
+	}
+
+	// Retrieve the instance from our store
+	f, ok := mfs[m.name]
+	if ok {
+		return f.mode, nil
+	}
+
+	// If the instance doesn't exist, return an error
+	return nil, fs.ErrNotExist
 }
 
 func (m *Memory) Read(p []byte) (n int, err error) {
-	m.Lock()
-	defer m.Unlock()
+	// Lock our instance
+	m.RLock()
+	defer m.RUnlock()
 
+	// If the instance has been closed, return an error
 	if m.closed {
 		return 0, os.ErrClosed
 	}
 
-	n = copy(p, m.b[m.offset:])
+	// Lock our filesystem
+	mufs.RLock()
+	defer mufs.RUnlock()
+
+	// Retrieve the underlying buffer represented by this instance
+	f, ok := mfs[m.name]
+	if !ok {
+		return 0, fs.ErrNotExist
+	}
+
+	// Copy the underlying buffer to the provided buffer, taking
+	// care to manage the instance's read offset.
+	n = copy(p, f.b[m.offset:])
 	if n == 0 {
 		err = io.EOF
 	}
@@ -200,9 +244,11 @@ func (m *Memory) Read(p []byte) (n int, err error) {
 }
 
 func (m *Memory) Close() error {
+	// Lock our instance
 	m.Lock()
 	defer m.Unlock()
 
+	// Set state
 	m.offset = 0
 	m.closed = true
 
@@ -210,28 +256,48 @@ func (m *Memory) Close() error {
 }
 
 func (m *Memory) Write(p []byte) (n int, err error) {
+	// Lock our instance
 	m.Lock()
 	defer m.Unlock()
 
+	// If the instance has been closed, return an error
 	if m.closed {
 		return 0, os.ErrClosed
 	}
-	if m.mode.perm&fs.FileMode(os.O_RDWR) == 0 {
+	// If the instance is not writable, return an error
+	if m.perm&fs.FileMode(os.O_RDWR) == 0 {
 		return 0, os.ErrPermission
 	}
 
-	m.b = append(m.b, p...)
+	// Lock our filesystem
+	mufs.Lock()
+	defer mufs.Unlock()
+
+	// Retrieve the underlying buffer represented by this instance
+	f, ok := mfs[m.name]
+	if !ok {
+		return 0, fs.ErrNotExist
+	}
+
+	// Append the provided buffer to the underlying buffer, taking
+	// care to manage the instance's write offset.
+	f.b = append(f.b, p...)
 	n = len(p)
 	m.offset += int64(n)
-	m.mode.size = int64(len(m.b))
-	m.mode.modtime = time.Now()
+
+	// Update the instance's metadata
+	f.mode.size = int64(len(f.b))
+	f.mode.modtime = time.Now()
+
 	return
 }
 
 func (m *Memory) Sync() error {
+	// Lock our instance
 	m.RLock()
 	defer m.RUnlock()
 
+	// If the instance has been closed, return an error
 	if m.closed {
 		return os.ErrClosed
 	}
@@ -240,37 +306,93 @@ func (m *Memory) Sync() error {
 }
 
 func (m *Memory) Seek(offset int64, whence int) (ret int64, err error) {
+	// Lock our instance
 	m.Lock()
 	defer m.Unlock()
 
+	// If the instance has been closed, return an error
 	if m.closed {
 		return 0, os.ErrClosed
 	}
 
+	// Lock our filesystem
+	mufs.RLock()
+	defer mufs.RUnlock()
+
+	// Retrieve the underlying buffer represented by this instance
+	f, ok := mfs[m.name]
+	if !ok {
+		return 0, fs.ErrNotExist
+	}
+
 	switch whence {
 	case 0:
+		// Seek relative to the origin of the file
 		m.offset = offset
 	case 1:
+		// Seek relative to the current offset
 		m.offset += offset
 	case 2:
-		m.offset = int64(len(m.b)) + offset
+		// Seek relative to the end of the file
+		m.offset = int64(len(f.b)) + offset
 	}
+
+	// If the offset is negative, return an error
+	if m.offset < 0 {
+		return 0, os.ErrInvalid
+	}
+
+	// If the offset is greater than the length of the underlying buffer,
+	// return an error
+	if m.offset > int64(len(f.b)) {
+		return 0, os.ErrInvalid
+	}
+
+	// Return the offset
 	return m.offset, nil
 }
 
 func (m *Memory) Truncate(size int64) error {
+	// If the size is negative, return an error
+	if size < 0 {
+		return os.ErrInvalid
+	}
+
+	// Lock our instance
 	m.Lock()
 	defer m.Unlock()
 
+	// If the instance has been closed, return an error
 	if m.closed {
 		return os.ErrClosed
 	}
-	if m.mode.perm&fs.FileMode(os.O_RDWR) == 0 {
+	// If the instance is not writable, return an error
+	if m.perm&fs.FileMode(os.O_RDWR) == 0 {
 		return os.ErrPermission
 	}
 
-	m.b = m.b[:size]
-	m.mode.size = int64(len(m.b))
-	m.mode.modtime = time.Now()
+	// Lock our filesystem
+	mufs.Lock()
+	defer mufs.Unlock()
+
+	// Retrieve the underlying buffer represented by this instance
+	f, ok := mfs[m.name]
+	if !ok {
+		return fs.ErrNotExist
+	}
+
+	// If the size is greater than the length of the underlying buffer,
+	// return an error
+	if size > int64(len(f.b)) {
+		return os.ErrInvalid
+	}
+
+	// Truncate the underlying buffer
+	f.b = f.b[:size]
+
+	// Update the instance's metadata
+	f.mode.size = int64(len(f.b))
+	f.mode.modtime = time.Now()
+
 	return nil
 }
