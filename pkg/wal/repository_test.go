82f82d1d8b3ac13e221b7244b60202f899167fbc
Jason Wilder <jawilder@microsoft.com>
2023-11-05 18:17:29 -0700

    Expand test coverage to memory provider

--- pkg/wal/repository_test.go
+++ pkg/wal/repository_test.go
@@ -11,74 +11,111 @@ import (
 )
 
 func TestRepository_Write(t *testing.T) {
-	dir := t.TempDir()
-	r := wal.NewRepository(wal.RepositoryOpts{
-		StorageDir: dir,
-	})
-	defer r.Close()
+	var providerTests = []struct {
+		Name            string
+		StorageProvider file.Provider
+	}{
+		{Name: "Disk", StorageProvider: &file.DiskProvider{}},
+		{Name: "Memory", StorageProvider: &file.MemoryProvider{}},
+	}
+	for _, tt := range providerTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			dir := t.TempDir()
+			r := wal.NewRepository(wal.RepositoryOpts{
+				StorageDir:      dir,
+				StorageProvider: tt.StorageProvider,
+			})
+			defer r.Close()
 
-	require.NoError(t, r.Open(context.Background()))
-	w, err := r.Get(context.Background(), []byte("foo"))
-	require.NoError(t, err)
-	require.NoError(t, w.Write(context.Background(), []byte("bar")))
+			require.NoError(t, r.Open(context.Background()))
+			w, err := r.Get(context.Background(), []byte("foo"))
+			require.NoError(t, err)
+			require.NoError(t, w.Write(context.Background(), []byte("bar")))
+		})
+	}
 }
 
 func TestRepository_Keys(t *testing.T) {
-	dir := t.TempDir()
-	r := wal.NewRepository(wal.RepositoryOpts{
-		StorageDir: dir,
-	})
-	defer r.Close()
+	var providerTests = []struct {
+		Name            string
+		StorageProvider file.Provider
+	}{
+		{Name: "Disk", StorageProvider: &file.DiskProvider{}},
+		{Name: "Memory", StorageProvider: &file.MemoryProvider{}},
+	}
 
-	require.NoError(t, r.Open(context.Background()))
+	for _, tt := range providerTests {
+		t.Run(tt.Name, func(t *testing.T) {
 
-	_, err := r.Get(context.Background(), []byte("foo"))
-	require.NoError(t, err)
+			dir := t.TempDir()
+			r := wal.NewRepository(wal.RepositoryOpts{
+				StorageDir:      dir,
+				StorageProvider: tt.StorageProvider,
+			})
+			defer r.Close()
 
-	_, err = r.Get(context.Background(), []byte("foo"))
-	require.NoError(t, err)
+			require.NoError(t, r.Open(context.Background()))
 
-	_, err = r.Get(context.Background(), []byte("bar"))
-	require.NoError(t, err)
+			_, err := r.Get(context.Background(), []byte("foo"))
+			require.NoError(t, err)
 
-	keys := r.Keys()
-	require.Equal(t, 2, len(keys))
-	require.Equal(t, "bar", string(keys[0]))
-	require.Equal(t, "foo", string(keys[1]))
+			_, err = r.Get(context.Background(), []byte("foo"))
+			require.NoError(t, err)
+
+			_, err = r.Get(context.Background(), []byte("bar"))
+			require.NoError(t, err)
+
+			keys := r.Keys()
+			require.Equal(t, 2, len(keys))
+			require.Equal(t, "bar", string(keys[0]))
+			require.Equal(t, "foo", string(keys[1]))
+		})
+	}
 }
 
 func TestRepository_Remove(t *testing.T) {
-	dir := t.TempDir()
-	r := wal.NewRepository(wal.RepositoryOpts{
-		StorageDir: dir,
-	})
-	defer r.Close()
+	var providerTests = []struct {
+		Name            string
+		StorageProvider file.Provider
+	}{
+		{Name: "Disk", StorageProvider: &file.DiskProvider{}},
+		{Name: "Memory", StorageProvider: &file.MemoryProvider{}},
+	}
+	for _, tt := range providerTests {
+		t.Run(tt.Name, func(t *testing.T) {
 
-	// Add a closed segment for this WAL.
-	seg, err := wal.NewSegment(dir, "foo", &file.DiskProvider{})
-	require.NoError(t, err)
-	require.NoError(t, seg.Close())
+			dir := t.TempDir()
+			r := wal.NewRepository(wal.RepositoryOpts{
+				StorageDir: dir,
+			})
+			defer r.Close()
 
-	require.NoError(t, r.Open(context.Background()))
-	w, err := r.Get(context.Background(), []byte("foo"))
-	require.NoError(t, err)
-	require.NoError(t, w.Write(context.Background(), []byte("bar")))
+			// Add a closed segment for this WAL.
+			seg, err := wal.NewSegment(dir, "foo", &file.DiskProvider{})
+			require.NoError(t, err)
+			require.NoError(t, seg.Close())
 
-	entries, err := os.ReadDir(dir)
-	require.NoError(t, err)
-	require.Equal(t, 2, len(entries))
+			require.NoError(t, r.Open(context.Background()))
+			w, err := r.Get(context.Background(), []byte("foo"))
+			require.NoError(t, err)
+			require.NoError(t, w.Write(context.Background(), []byte("bar")))
 
-	// Expect an error trying remove WAL that is still open.
-	require.Error(t, r.Remove([]byte("foo")))
+			entries, err := os.ReadDir(dir)
+			require.NoError(t, err)
+			require.Equal(t, 2, len(entries))
 
-	// WAL must be closed before we can remove it.
-	require.NoError(t, w.Close())
+			// Expect an error trying remove WAL that is still open.
+			require.Error(t, r.Remove([]byte("foo")))
 
-	require.NoError(t, r.Remove([]byte("foo")))
-	require.Equal(t, 0, len(r.Keys()))
+			// WAL must be closed before we can remove it.
+			require.NoError(t, w.Close())
 
-	entries, err = os.ReadDir(dir)
-	require.NoError(t, err)
-	require.Equal(t, 0, len(entries))
+			require.NoError(t, r.Remove([]byte("foo")))
+			require.Equal(t, 0, len(r.Keys()))
 
+			entries, err = os.ReadDir(dir)
+			require.NoError(t, err)
+			require.Equal(t, 0, len(entries))
+		})
+	}
 }
