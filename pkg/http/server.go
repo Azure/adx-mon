47068d8140e5f95434dc9b0344796f01f9eb98d7
Jason Wilder <jawilder@microsoft.com>
2023-11-13 20:13:07 -0700

    Move http server to pkg

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -1,4 +1,4 @@
-package collector
+package http
 
 import (
 	"context"
@@ -9,19 +9,21 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type HttpServerOpts struct {
+type ServerOpts struct {
 	ListenAddr string
 }
 
+// HttpServer is a http server that is preconfigured with a prometheus metrics handler.  Additional handlers can be
+// registered with RegisterHandler.
 type HttpServer struct {
 	mux  *http.ServeMux
-	opts *HttpServerOpts
+	opts *ServerOpts
 	srv  *http.Server
 
 	cancelFn context.CancelFunc
 }
 
-func NewHttpServer(opts *HttpServerOpts) *HttpServer {
+func NewServer(opts *ServerOpts) *HttpServer {
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	return &HttpServer{
@@ -30,6 +32,7 @@ func NewHttpServer(opts *HttpServerOpts) *HttpServer {
 	}
 }
 
+// Open starts the http server.
 func (s *HttpServer) Open(ctx context.Context) error {
 	s.srv = &http.Server{Addr: s.opts.ListenAddr, Handler: s.mux}
 
@@ -42,10 +45,12 @@ func (s *HttpServer) Open(ctx context.Context) error {
 	return nil
 }
 
+// Close shuts down the http server.
 func (s *HttpServer) Close() error {
 	return s.srv.Shutdown(context.Background())
 }
 
+// RegisterHandler registers a new handler at the given path.  The handler must be registered before Open is called.
 func (s *HttpServer) RegisterHandler(path string, handlerFunc http.HandlerFunc) {
 	s.mux.Handle(path, handlerFunc)
 }
