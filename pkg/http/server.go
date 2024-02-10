1985aa058f6c91999a134d19b04fd6a3c56f211b
Jason Wilder <jawilder@microsoft.com>
2024-02-10 15:23:35 -0700

    Add HTTP server timeouts by default
    This adds HTTP server timeouts to protect against slow/problem
    clients.

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/net/netutil"
@@ -19,7 +20,25 @@ type HttpHandler struct {
 type ServerOpts struct {
 	// MaxConns is the maximum number of connections the server will accept.  This value is only respected if
 	// Listener is nil.
-	MaxConns   int
+	MaxConns int
+
+	// ReadTimeout is the maximum duration before timing out read of the request.
+	// If not specified, the default is 5 seconds.
+	ReadTimeout time.Duration
+
+	// WriteTimeout is the maximum duration before timing out write of the response.
+	// If not specified, the default is 10 seconds.
+	WriteTimeout time.Duration
+
+	// IdleTimeout is the maximum amount of time to wait for the next request when keep-alives are enabled.
+	// If not specified, the default is 10 seconds.
+	IdleTimeout time.Duration
+
+	// MaxHeaderBytes is the maximum number of bytes the server will read parsing the request header's keys and values,
+	// If not specified, the default is 1MB (1 << 20).
+	MaxHeaderBytes int
+
+	// ListenAddr is the address to listen on.  If a Listener is provided, this value is ignored.
 	ListenAddr string
 
 	// Listener is the listener to use.  If nil, a new listener will be created.
@@ -55,12 +74,39 @@ func (s *HttpServer) Open(ctx context.Context) error {
 		if err != nil {
 			return err
 		}
-		if s.opts.MaxConns > 0 {
-			s.listener = netutil.LimitListener(s.listener, s.opts.MaxConns)
-		}
 	}
 
-	s.srv = &http.Server{Addr: s.opts.ListenAddr, Handler: s.mux}
+	if s.opts.MaxConns > 0 {
+		s.listener = netutil.LimitListener(s.listener, s.opts.MaxConns)
+	}
+
+	readTimeout := 5 * time.Second
+	if s.opts.ReadTimeout > 0 {
+		readTimeout = s.opts.ReadTimeout
+	}
+
+	writeTimeout := 10 * time.Second
+	if s.opts.WriteTimeout > 0 {
+		writeTimeout = s.opts.WriteTimeout
+	}
+
+	idleTimeout := 10 * time.Second
+	if s.opts.IdleTimeout > 0 {
+		idleTimeout = s.opts.IdleTimeout
+	}
+
+	maxHeaderBytes := 1 << 20
+	if s.opts.MaxHeaderBytes > 0 {
+		maxHeaderBytes = s.opts.MaxHeaderBytes
+	}
+
+	s.srv = &http.Server{
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		IdleTimeout:    idleTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+		Addr:           s.opts.ListenAddr,
+		Handler:        s.mux}
 
 	go func() {
 		if err := s.srv.Serve(s.listener); err != nil && err != http.ErrServerClosed {
