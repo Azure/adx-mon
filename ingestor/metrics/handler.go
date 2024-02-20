01929b5caebcbe325c03bd6372d5051cd8f247d3
Mike Keesey <mikekeesey@microsoft.com>
2024-02-20 16:07:57 -0700

    Update metric handler ingestion metric to use configured path

--- ingestor/metrics/handler.go
+++ ingestor/metrics/handler.go
@@ -82,7 +82,7 @@ func NewHandler(opts HandlerOpts) *Handler {
 
 // HandleReceive handles the prometheus remote write requests and writes them to the store.
 func (s *Handler) HandleReceive(w http.ResponseWriter, r *http.Request) {
-	m := metrics.RequestsReceived.MustCurryWith(prometheus.Labels{"path": "/receive"})
+	m := metrics.RequestsReceived.MustCurryWith(prometheus.Labels{"path": s.Path})
 	defer func() {
 		if err := r.Body.Close(); err != nil {
 			logger.Errorf("close http body: %s", err.Error())
