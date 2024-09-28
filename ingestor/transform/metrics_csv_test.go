4d403b9eec10258b6b4f976463f2f75d49e6bdc5
Jason Wilder <jawilder@microsoft.com>
2024-09-27 22:23:08 -0600

    Embed schema in CSV header

--- ingestor/transform/metrics_csv_test.go
+++ ingestor/transform/metrics_csv_test.go
@@ -49,7 +49,8 @@ func TestMetricsCSVWriter_MarshalCSV(t *testing.T) {
 	w := NewMetricsCSVWriter(&b, nil)
 	err := w.MarshalCSV(ts)
 	require.NoError(t, err)
-	require.Equal(t, `2022-11-22T10:22:04.001Z,-9070404444212865161,"{""measurement"":""used_cpu_user_children"",""hostname"":""host_1"",""region"":""eastus""}",0.000000000
+	require.Equal(t, `Timestamp:datetime,SeriesId:long,Labels:dynamic,Value:real
+2022-11-22T10:22:04.001Z,-9070404444212865161,"{""measurement"":""used_cpu_user_children"",""hostname"":""host_1"",""region"":""eastus""}",0.000000000
 2022-11-22T10:22:05.002Z,-9070404444212865161,"{""measurement"":""used_cpu_user_children"",""hostname"":""host_1"",""region"":""eastus""}",1.000000000
 2022-11-22T10:22:06.003Z,-9070404444212865161,"{""measurement"":""used_cpu_user_children"",""hostname"":""host_1"",""region"":""eastus""}",2.000000000
 `, string(w.Bytes()))
@@ -94,7 +95,11 @@ func BenchmarkMetricsCSVWriter_MarshalCSV(b *testing.B) {
 	}
 
 	buf := bytes.NewBuffer(make([]byte, 0, 64*1024))
-	w := NewMetricsCSVWriter(buf, []string{"region", "Hostname", "bar"})
+	w := NewMetricsCSVWriter(buf, []Field{
+		{Name: "Region", Source: "region", Type: "string"},
+		{Name: "Hostname", Source: "hostname", Type: "string"},
+		{Name: "Bar", Source: "bar", Type: "string"},
+	})
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -133,11 +138,18 @@ func TestMetricsCSVWriter_MarshalCSV_LiftLabel(t *testing.T) {
 	}
 
 	var b bytes.Buffer
-	w := NewMetricsCSVWriter(&b, []string{"zip", "zap", "region", "Hostname", "bar"})
+	w := NewMetricsCSVWriter(&b, []Field{
+		{Name: "Zip", Source: "zip", Type: "string"},
+		{Name: "Zap", Source: "zap", Type: "string"},
+		{Name: "Region", Source: "region", Type: "string"},
+		{Name: "Hostname", Source: "hostname", Type: "string"},
+		{Name: "Bar", Source: "bar", Type: "string"},
+	})
 
 	err := w.MarshalCSV(ts)
 	require.NoError(t, err)
-	require.Equal(t, `2022-11-22T10:22:04.001Z,1265838189064375029,,host_1,eastus,,,"{""measurement"":""used_cpu_user_children""}",0.000000000
+	require.Equal(t, `Timestamp:datetime,SeriesId:long,Labels:dynamic,Value:real,Zip:string,Zap:string,Region:string,Hostname:string,Bar:string
+2022-11-22T10:22:04.001Z,1265838189064375029,,host_1,eastus,,,"{""measurement"":""used_cpu_user_children""}",0.000000000
 `, b.String())
 }
 
