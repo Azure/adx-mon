d7ae56b881ad637014a3f62d667e2894d56d5380
Mike Keesey <mikekeesey@microsoft.com>
2024-10-30 17:44:43 -0600

    Adding e2e test for journal source

--- collector/logs/sinks/counting.go
+++ collector/logs/sinks/counting.go
@@ -10,6 +10,7 @@ import (
 type CountingSink struct {
 	expectedCount int64
 	currentCount  int64
+	latest        *types.Log
 
 	lock        sync.Mutex
 	done        bool
@@ -20,6 +21,7 @@ func NewCountingSink(expectedCount int64) *CountingSink {
 	return &CountingSink{
 		expectedCount: expectedCount,
 		currentCount:  0,
+		latest:        nil,
 		done:          false,
 		doneChannel:   make(chan int64, 1),
 	}
@@ -32,6 +34,7 @@ func (s *CountingSink) Open(ctx context.Context) error {
 func (s *CountingSink) Send(ctx context.Context, batch *types.LogBatch) error {
 	s.lock.Lock()
 	s.currentCount += int64(len(batch.Logs))
+	s.latest = batch.Logs[len(batch.Logs)-1]
 	batch.Ack()
 	if !s.done && s.currentCount >= s.expectedCount {
 		s.done = true
@@ -52,4 +55,10 @@ func (s *CountingSink) Name() string {
 
 func (s *CountingSink) DoneChan() chan int64 {
 	return s.doneChannel
+}
+
+func (s *CountingSink) Latest() *types.Log {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.latest
 }
