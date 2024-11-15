f149851c6be4eb3ce6298c6cadb7d76f19a07f02
Jesse Thompson <jesse.thompson@microsoft.com>
2024-11-15 19:56:41 +0000

    Integration test for CRD/Functions

---
+++ pkg/testutils/fs.go
@@ -0,0 +1,50 @@
+package testutils
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func GetGitRootDir() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(out.String()), nil
+}
+
+func CopyFile(src, dst string) error {
+	// Open the source file
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	// Create the destination file
+	destinationFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	// Copy the contents from source to destination
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	// Flush the contents to the destination file
+	err = destinationFile.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
