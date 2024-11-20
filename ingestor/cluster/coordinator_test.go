d48a2f60f947fb0c098f3e2d04a0433543220cc4
Jesse Thompson <Jesse.Thompson@microsoft.com>
2024-11-20 08:54:59 -0700

    Integration tests for Coordinator and CRDs (#445)
    * Integration tests for CRDs
    
    * Integration tests for Coordinator

--- ingestor/cluster/coordinator_test.go
+++ ingestor/cluster/coordinator_test.go
@@ -3,13 +3,22 @@ package cluster
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/Azure/adx-mon/pkg/otlp"
+	"github.com/Azure/adx-mon/pkg/prompb"
+	"github.com/Azure/adx-mon/pkg/testutils"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/k3s"
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
 	fakek8s "k8s.io/client-go/kubernetes/fake"
 	v12 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func TestCoordinator_NewPeer(t *testing.T) {
@@ -249,4 +258,97 @@ func (l *fakePodLister) List(selector labels.Selector) (ret []*v1.Pod, err error
 
 func (l *fakePodLister) Pods(namespace string) v12.PodNamespaceLister {
 	return l
+}
+
+func TestCoordinatorInK8s(t *testing.T) {
+	// This is an integration test where a Coordinator is created using a k3s cluster
+	// configuration and a statefulset is then created that matches the Coordinator's
+	// peer predicate. What we're testing is that the Coordinator is correctly utilizing
+	// a k8s informer and its predicate is correctly identifying peer pods and its leader state.
+	otlpWriter := func(ctx context.Context, database, table string, logs *otlp.Logs) error {
+		t.Helper()
+		return nil
+	}
+
+	timeSeriesWriter := func(ctx context.Context, ts []*prompb.TimeSeries) error {
+		t.Helper()
+		return nil
+	}
+
+	ctx := context.Background()
+	k3sContainer, err := k3s.Run(ctx, "rancher/k3s:v1.31.2-k3s1")
+	testcontainers.CleanupContainer(t, k3sContainer)
+	require.NoError(t, err)
+
+	kubeconfig, err := testutils.WriteKubeConfig(ctx, k3sContainer, t.TempDir())
+	require.NoError(t, err)
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	require.NoError(t, err)
+
+	client, err := kubernetes.NewForConfig(config)
+	require.NoError(t, err)
+
+	opts := &CoordinatorOpts{
+		WriteTimeSeriesFn:  timeSeriesWriter,
+		WriteOTLPLogsFn:    otlpWriter,
+		K8sCli:             client,
+		Namespace:          "test-namespace",
+		Hostname:           "ingestor-0",
+		InsecureSkipVerify: true,
+	}
+	c, err := NewCoordinator(opts)
+	require.NoError(t, err)
+
+	require.NoError(t, c.Open(ctx))
+
+	ns := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: opts.Namespace,
+		},
+	}
+	_, err = client.CoreV1().Namespaces().Create(ctx, ns, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	replicas := int32(1)
+	ss := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "ingestor",
+			Namespace: opts.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "ingestor",
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "ingestor",
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  "ingestor",
+							Image: "mcr.microsoft.com/cbl-mariner/base/nginx:1.22-cm2.0",
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = client.AppsV1().StatefulSets(opts.Namespace).Create(ctx, ss, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	require.NoError(t, c.WriteOTLPLogs(ctx, "test-database", "test-table", &otlp.Logs{}))
+	require.NoError(t, c.Write(ctx, &prompb.WriteRequest{}))
+
+	require.Eventually(t, func() bool {
+		return c.IsLeader()
+	}, time.Minute, 100*time.Millisecond)
+
+	require.NoError(t, c.Close())
 }
--- pkg/crd/crd.go
+++ pkg/crd/crd.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/Azure/adx-mon/pkg/logger"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -15,9 +16,10 @@ type Store interface {
 }
 
 type Options struct {
-	CtrlCli client.Client
-	List    client.ObjectList
-	Store   Store
+	CtrlCli       client.Client
+	List          client.ObjectList
+	Store         Store
+	PollFrequency time.Duration
 }
 
 type CRD struct {
@@ -53,12 +55,15 @@ func (c *CRD) Open(ctx context.Context) error {
 		return fmt.Errorf("failed to store objects: %w", err)
 	}
 
+	if c.opts.PollFrequency == 0 {
+		c.opts.PollFrequency = time.Minute
+	}
 	go func() {
 		for {
 			select {
 			case <-c.ctx.Done():
 				return
-			case <-time.After(time.Minute):
+			case <-time.After(c.opts.PollFrequency):
 				list, err := c.List(ctx)
 				if err != nil {
 					logger.Errorf("Failed to list objects: %s", err.Error())
@@ -80,10 +85,12 @@ func (c *CRD) Close() error {
 	c.cancel()
 	return nil
 }
-
 func (c *CRD) List(ctx context.Context) (client.ObjectList, error) {
 	list := c.opts.List.DeepCopyObject().(client.ObjectList)
 	if err := c.opts.CtrlCli.List(ctx, list); err != nil {
+		if errors.Is(err, &meta.NoKindMatchError{}) {
+			return list, nil
+		}
 		return nil, fmt.Errorf("failed to list CRDs: %w", err)
 	}
 
