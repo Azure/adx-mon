b1d112c9db45bad34833c1e0d634b44619704d7f
Jesse Thompson <jesse.thompson@microsoft.com>
2024-12-12 16:33:21 +0000

    Combine the functionality of storage and crd
    There is an extra bit of unecessary abstraction that complicates our Function handling where our storage package is acting as a glue between the crd and adx packages. By combining the storage and crd package it's far easier to understand the code's flow. Furthermore, the code that lives in pkg is generally meant to be shared by more than one other package, so until that's necessary, having a crd package doesn't fit the intent of pkg.

--- ingestor/storage/kql_functions_test.go
+++ ingestor/storage/kql_functions_test.go
@@ -2,21 +2,25 @@ package storage_test
 
 import (
 	"context"
-	"os"
+	"errors"
 	"path/filepath"
 	"testing"
 	"time"
 
-	v1 "github.com/Azure/adx-mon/api/v1"
-	"github.com/Azure/adx-mon/ingestor/storage"
 	"github.com/Azure/adx-mon/pkg/testutils"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/k3s"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	adxmonv1 "github.com/Azure/adx-mon/api/v1"
+	"github.com/Azure/adx-mon/ingestor/storage"
 )
 
 func TestFunctions(t *testing.T) {
@@ -24,12 +28,10 @@ func TestFunctions(t *testing.T) {
 
 	scheme := clientgoscheme.Scheme
 	require.NoError(t, clientgoscheme.AddToScheme(scheme))
-	require.NoError(t, v1.AddToScheme(scheme))
+	require.NoError(t, adxmonv1.AddToScheme(scheme))
 
 	crdPath := filepath.Join(t.TempDir(), "crd.yaml")
 	require.NoError(t, testutils.CopyFile("../../kustomize/bases/functions_crd.yaml", crdPath))
-	fnCrdPath := filepath.Join(t.TempDir(), "fn-crd.yaml")
-	os.WriteFile(fnCrdPath, []byte(crd), 0644)
 
 	ctx := context.Background()
 	k3sContainer, err := k3s.Run(ctx, "rancher/k3s:v1.31.2-k3s1")
@@ -37,7 +39,6 @@ func TestFunctions(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NoError(t, k3sContainer.CopyFileToContainer(ctx, crdPath, filepath.Join(testutils.K3sManifests, "crd.yaml"), 0644))
-	require.NoError(t, k3sContainer.CopyFileToContainer(ctx, fnCrdPath, filepath.Join(testutils.K3sManifests, "fn-crd.yaml"), 0644))
 
 	kubeconfig, err := testutils.WriteKubeConfig(ctx, k3sContainer, t.TempDir())
 	require.NoError(t, err)
@@ -48,30 +49,116 @@ func TestFunctions(t *testing.T) {
 	ctrlCli, err := ctrlclient.New(config, ctrlclient.Options{})
 	require.NoError(t, err)
 
-	functionStore := storage.NewFunctions(ctrlCli)
-	list := &v1.FunctionList{}
+	require.Eventually(t, func() bool {
+		crd := &adxmonv1.Function{}
+		err := ctrlCli.Get(ctx, types.NamespacedName{Namespace: "default"}, crd)
+		return !errors.Is(err, &meta.NoKindMatchError{})
+	}, time.Minute, time.Second)
 
-	t.Run("Get Functions", func(t *testing.T) {
-		require.Eventually(t, func() bool {
-			err := ctrlCli.List(ctx, list)
-			return err == nil && len(list.Items) > 0
-		}, time.Minute, time.Second)
+	functionStore := storage.NewFunctions(ctrlCli, nil)
+
+	resourceName := "testtest"
+	typeNamespacedName := types.NamespacedName{
+		Name:      resourceName,
+		Namespace: "default",
+	}
+
+	t.Run("Install function", func(t *testing.T) {
+		fn := &adxmonv1.Function{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      typeNamespacedName.Name,
+				Namespace: typeNamespacedName.Namespace,
+			},
+			Spec: adxmonv1.FunctionSpec{
+				Body:     "some-function-body",
+				Database: "some-database",
+			},
+		}
+		require.NoError(t, ctrlCli.Create(ctx, fn))
 	})
 
-	t.Run("Function is installed", func(t *testing.T) {
-		require.Eventually(t, func() bool {
-			require.NoError(t, functionStore.Receive(ctx, list))
-			return len(functionStore.List()) == 1
-		}, time.Minute, time.Second)
+	t.Run("List Functions", func(t *testing.T) {
+		fns, err := functionStore.List(ctx)
+		require.NoError(t, err)
+		require.Len(t, fns, 1)
+		require.Equal(t, int64(1), fns[0].GetGeneration())
+		require.Equal(t, int64(0), fns[0].Status.ObservedGeneration)
+	})
+
+	t.Run("Does not included suspended", func(t *testing.T) {
+		fn := &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+
+		fn.Spec.Suspend = new(bool)
+		*fn.Spec.Suspend = true
+		require.NoError(t, ctrlCli.Update(ctx, fn))
+
+		fns, err := functionStore.List(ctx)
+		require.NoError(t, err)
+		require.Empty(t, fns)
+
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+		*fn.Spec.Suspend = false
+		require.NoError(t, ctrlCli.Update(ctx, fn))
+	})
+
+	t.Run("Updates status", func(t *testing.T) {
+		fn := &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+
+		fn.Status.Status = adxmonv1.Success
+		require.NoError(t, functionStore.UpdateStatus(ctx, fn))
+
+		fn = &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+		require.Equal(t, fn.Status.Status, adxmonv1.Success)
+		require.Equal(t, fn.Status.ObservedGeneration, fn.GetGeneration())
+		require.False(t, fn.Status.LastTimeReconciled.IsZero())
+
+		fns, err := functionStore.List(ctx)
+		require.NoError(t, err)
+		require.Empty(t, fns) // because the generation is up to date
+	})
+
+	t.Run("Does not update generation upon failure", func(t *testing.T) {
+		fn := &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+
+		currentGeneration := fn.GetGeneration()
+		fn.Status.Status = adxmonv1.Failed
+		require.NoError(t, functionStore.UpdateStatus(ctx, fn))
+
+		fn = &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+		require.Equal(t, fn.Status.Status, adxmonv1.Failed)
+		require.Equal(t, currentGeneration, fn.GetGeneration())
+	})
+
+	t.Run("Respects leader election", func(t *testing.T) {
+		coord := &elector{isLeader: true}
+		functionStore := storage.NewFunctions(ctrlCli, coord)
+
+		// Update the function to ensure the generation is updated
+		fn := &adxmonv1.Function{}
+		require.NoError(t, ctrlCli.Get(ctx, typeNamespacedName, fn))
+		fn.Spec.Database = "some-other-database"
+		require.NoError(t, ctrlCli.Update(ctx, fn))
+
+		fns, err := functionStore.List(ctx)
+		require.NoError(t, err)
+		require.Len(t, fns, 1)
+
+		coord.isLeader = false
+		fns, err = functionStore.List(ctx)
+		require.NoError(t, err)
+		require.Empty(t, fns)
 	})
 }
 
-var crd = `---
-apiVersion: adx-mon.azure.com/v1
-kind: Function
-metadata:
-  name: some-crd
-spec:
-  body: some-function-body
-  database: some-database
----`
+type elector struct {
+	isLeader bool
+}
+
+func (e *elector) IsLeader() bool {
+	return e.isLeader
+}
