2f25b0904c628bf69de07ea342ef0a454ea9b25c
Jesse Thompson <Jesse.Thompson@microsoft.com>
2024-12-17 13:53:54 -0700

    Add leader election to Functions processing (#480)
    * Add "suspended" and "observedGeneration" to the funtions CRD
    
    The suspended property enables an operator to suspend processing of a CRD during some investigative period.
    
    The ObservedGeneration property enables us to remain stateless with respect to our processing model. So instead of having to maintain a cache so that we can determine when a CRD has been updated, we can remove the cache and know if a CRD requires reconciling by comparing its ObservedGeneration to the current Generation.
    
    * Delegate Function CRD handling to tasks
    
    There's no need to be reconciling Functions in more than one place. We already periodically reconcile functions in our task handler, so there's no need to do so in the uploader, too.
    
    * Combine the functionality of storage and crd
    
    There is an extra bit of unecessary abstraction that complicates our Function handling where our storage package is acting as a glue between the crd and adx packages. By combining the storage and crd package it's far easier to understand the code's flow. Furthermore, the code that lives in pkg is generally meant to be shared by more than one other package, so until that's necessary, having a crd package doesn't fit the intent of pkg.
    
    * Integrate new function store to task management
    
    Several unit tests were removed for this commit due to the shift of responsibility with respect to ObservedGeneration and LastReconcileTime, which was moved from task management to the function store layer. The reason for moving the responsibility for these two fields is that it's a bit of an implementation leak for the kusto layer to have to understand how and why the ObservedGeneration and LastReconciledTime are utilized.
    
    * Integrate the new function store
    
    * Remove unused CRD tool
    
    * Drop KQL Functions upon CRD deletion

--- ingestor/storage/kql_functions.go
+++ ingestor/storage/kql_functions.go
@@ -2,126 +2,132 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"sync"
 
-	v1 "github.com/Azure/adx-mon/api/v1"
 	"github.com/Azure/adx-mon/pkg/logger"
+	"github.com/Azure/adx-mon/pkg/scheduler"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	adxmonv1 "github.com/Azure/adx-mon/api/v1"
 )
 
-type Functions struct {
-	mu        sync.RWMutex
-	views     *v1.FunctionList
-	functions *v1.FunctionList
-	client    client.StatusClient
+const (
+	// name of our custom finalizer
+	FinalizerName = "function.adx-mon.azure.com/finalizer"
+)
+
+type Functions interface {
+	UpdateStatus(ctx context.Context, fn *adxmonv1.Function) error
+	List(ctx context.Context) ([]*adxmonv1.Function, error)
 }
 
-func NewFunctions(client client.StatusClient) *Functions {
-	return &Functions{
-		client: client,
+type functions struct {
+	Client  client.Client
+	Elector scheduler.Elector
+}
+
+func NewFunctions(client client.Client, elector scheduler.Elector) *functions {
+	return &functions{
+		Client:  client,
+		Elector: elector,
 	}
 }
 
-func (f *Functions) UpdateStatus(ctx context.Context, fn *v1.Function) error {
-	if f.client == nil {
+func (f *functions) UpdateStatus(ctx context.Context, fn *adxmonv1.Function) error {
+	if f.Client == nil {
 		return fmt.Errorf("no client provided")
 	}
 
-	return f.client.Status().Update(ctx, fn)
+	if fn.Status.Status == adxmonv1.Success {
+		fn.Status.ObservedGeneration = fn.GetGeneration()
+
+		if !fn.DeletionTimestamp.IsZero() && controllerutil.ContainsFinalizer(fn, FinalizerName) {
+			// remove our finalizer from the list and update it.
+			controllerutil.RemoveFinalizer(fn, FinalizerName)
+			if err := f.Client.Update(ctx, fn); err != nil {
+				logger.Errorf("Failed to remove finalizer from function %s: %v", fn.Name, err)
+				fn.Status.Status = adxmonv1.Failed
+			} else {
+				return nil
+			}
+		}
+	}
+
+	fn.Status.LastTimeReconciled = metav1.Now()
+	return f.Client.Status().Update(ctx, fn)
 }
 
-func (f *Functions) Delete(ctx context.Context, fn *v1.Function) error {
-	if f.client == nil {
+func (f *functions) List(ctx context.Context) ([]*adxmonv1.Function, error) {
+	if f.Client == nil {
+		return nil, fmt.Errorf("no client provided")
+	}
+
+	if f.Elector != nil && !f.Elector.IsLeader() {
+		return nil, nil
+	}
+
+	list := &adxmonv1.FunctionList{}
+	if err := f.Client.List(ctx, list); err != nil {
+		if errors.Is(err, &meta.NoKindMatchError{}) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to list functions: %w", err)
+	}
+
+	var fns []*adxmonv1.Function
+	for _, fn := range list.Items {
+		if fn.Spec.Suspend != nil && *fn.Spec.Suspend {
+			// Skip suspended functions
+			continue
+		}
+
+		if !fn.GetDeletionTimestamp().IsZero() {
+			fn.Status.Reason = "Function deleted"
+
+		} else {
+
+			switch fn.GetGeneration() {
+			case fn.Status.ObservedGeneration:
+				// Skip functions that are up to date
+				continue
+
+			case 1:
+				fn.Status.Reason = "Function created"
+
+			default:
+				fn.Status.Reason = "Function updated"
+			}
+
+			if err := f.ensureFinalizer(ctx, &fn); err != nil {
+				logger.Errorf("Failed to ensure finalizer for function %s: %v", fn.Name, err)
+			}
+		}
+
+		fns = append(fns, &fn)
+	}
+
+	return fns, nil
+}
+
+func (f *functions) ensureFinalizer(ctx context.Context, fn *adxmonv1.Function) error {
+	if f.Client == nil {
 		return fmt.Errorf("no client provided")
 	}
 
-	return nil
-}
-
-func (f *Functions) View(database, table string) (*v1.Function, bool) {
-	f.mu.RLock()
-	defer f.mu.RUnlock()
-
-	if f.views == nil {
-		return nil, false
-	}
-
-	// TODO (jesthom): Once the parser is in place we can identify the Views.
-
-	return nil, false
-}
-
-func (f *Functions) Functions() []*v1.Function {
-	f.mu.RLock()
-	defer f.mu.RUnlock()
-
-	if f.functions == nil {
-		return nil
-	}
-
-	var fns []*v1.Function
-	for _, f := range f.functions.Items {
-		fns = append(fns, f.DeepCopy())
-	}
-	return fns
-}
-
-func (f *Functions) List() []*v1.Function {
-	f.mu.RLock()
-	defer f.mu.RUnlock()
-
-	var fns []*v1.Function
-	if f.functions != nil {
-		for _, fn := range f.functions.Items {
-			fns = append(fns, fn.DeepCopy())
+	// examine DeletionTimestamp to determine if object is under deletion
+	if fn.ObjectMeta.DeletionTimestamp.IsZero() {
+		// The object is not being deleted, so if it does not have our finalizer,
+		// then lets add the finalizer and update the object. This is equivalent
+		// to registering our finalizer.
+		if !controllerutil.ContainsFinalizer(fn, FinalizerName) {
+			controllerutil.AddFinalizer(fn, FinalizerName)
+			return f.Client.Update(ctx, fn)
 		}
 	}
-	if f.views != nil {
-		for _, view := range f.views.Items {
-			fns = append(fns, view.DeepCopy())
-		}
-	}
-
-	return fns
-}
-
-func (f *Functions) Receive(ctx context.Context, list client.ObjectList) error {
-	items, ok := list.(*v1.FunctionList)
-	if !ok {
-		return fmt.Errorf("expected *v1.FunctionList, got %T", list)
-	}
-	if items == nil || len(items.Items) == 0 {
-		return nil
-	}
-
-	var (
-		views     = &v1.FunctionList{}
-		functions = &v1.FunctionList{}
-		unique    = make(map[string]struct{})
-	)
-	for _, function := range items.Items {
-		// TODO (jesthom): If a database isn't specified, should we just consider installing
-		// the function in all tracked databases? This would be useful for adx-mon's
-		// own functions, for example.
-		if function.Spec.Database == "" {
-			logger.Errorf("Function %s has no database", function.Name)
-			continue
-		}
-		// TODO (jesthom): Once the parser is in place we can perform more validation.
-		if _, ok := unique[function.Spec.Database+function.Name]; ok {
-			logger.Errorf("Function %s is a duplicate", function.Name)
-			continue
-		}
-		unique[function.Spec.Database+function.Name] = struct{}{}
-		// TODO (jesthom): Once the parser is in place separate out the Views from the Functions.
-		functions.Items = append(functions.Items, *function.DeepCopy())
-	}
-
-	f.mu.Lock()
-	f.views = views
-	f.functions = functions
-	f.mu.Unlock()
 
 	return nil
 }
