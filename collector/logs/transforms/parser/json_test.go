9ccd8c1475215a7cb8af8ba2383878c115a9df82
Mike Keesey <mikekeesey@microsoft.com>
2025-02-06 20:09:44 +0000

    Json parser should fail when not provided json
    Not doing so prevents falling back to other parsers.

--- collector/logs/transforms/parser/json_test.go
+++ collector/logs/transforms/parser/json_test.go
@@ -8,14 +8,53 @@ import (
 )
 
 func TestJsonParse(t *testing.T) {
+	type testcase struct {
+		name         string
+		input        string
+		expectedBody map[string]interface{}
+		expectErr    bool
+	}
+
+	tests := []testcase{
+		{
+			name:         "empty",
+			input:        "",
+			expectedBody: map[string]interface{}{},
+			expectErr:    true,
+		},
+		{
+			name:         "invalid plain string",
+			input:        "not-a-string",
+			expectedBody: map[string]interface{}{},
+			expectErr:    true,
+		},
+		{
+			name:         "invalid json",
+			input:        "{",
+			expectedBody: map[string]interface{}{},
+			expectErr:    true,
+		},
+		{
+			name:         "valid json",
+			input:        `{"a": 1, "b": "2", "c": {"d": 3}}`,
+			expectedBody: map[string]interface{}{"a": 1.0, "b": "2", "c": map[string]interface{}{"d": 3.0}},
+			expectErr:    false,
+		},
+	}
+
 	parser, _ := NewJsonParser(JsonParserConfig{})
-	msg := `{"a": 1, "b": "2", "c": {"d": 3}}`
-	log := types.NewLog()
-	err := parser.Parse(log, msg)
-	require.NoError(t, err)
-	require.Equal(t, 1.0, log.Body["a"])
-	require.Equal(t, "2", log.Body["b"])
-	require.Equal(t, map[string]interface{}{"d": 3.0}, log.Body["c"])
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			log := types.NewLog()
+			err := parser.Parse(log, tt.input)
+			if tt.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectedBody, log.Body)
+			}
+		})
+	}
 }
 
 func BenchmarkJsonParse(b *testing.B) {
