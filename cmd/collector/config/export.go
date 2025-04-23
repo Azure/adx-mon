4568c03bd372756e9ecbd666765053633252af12
Mike Keesey <mikekeesey@microsoft.com>
2025-04-23 22:00:07 +0000

    Add config for fluent-forward log exporter

--- cmd/collector/config/export.go
+++ cmd/collector/config/export.go
@@ -2,15 +2,20 @@ package config
 
 import (
 	"fmt"
+	"net"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/Azure/adx-mon/collector/export"
+	"github.com/Azure/adx-mon/collector/logs/types"
 	"github.com/Azure/adx-mon/pkg/remote"
 	"github.com/Azure/adx-mon/transform"
 )
 
 type Exporters struct {
-	OtlpMetricExport []*OtlpMetricExport `toml:"otlp-metric-export" comment:"Configuration for exporting metrics to an OTLP/HTTP endpoint."`
+	OtlpMetricExport       []*OtlpMetricExport       `toml:"otlp-metric-export" comment:"Configuration for exporting metrics to an OTLP/HTTP endpoint."`
+	FluentForwardLogExport []*FluentForwardLogExport `toml:"fluent-forward-log-export" comment:"Configuration for exporting logs to a Fluentd/Fluent Bit endpoint."`
 }
 
 func (e *Exporters) Validate() error {
@@ -21,6 +26,15 @@ func (e *Exporters) Validate() error {
 		}
 		if _, ok := exporterNames[exporter.Name]; ok {
 			return fmt.Errorf("exporter.otlp-metric-export[%d].name %q is not unique", i, exporter.Name)
+		}
+		exporterNames[exporter.Name] = struct{}{}
+	}
+	for i, exporter := range e.FluentForwardLogExport {
+		if err := exporter.Validate(); err != nil {
+			return fmt.Errorf("exporter.fluent-forward-log-export[%d].%w", i, err)
+		}
+		if _, ok := exporterNames[exporter.Name]; ok {
+			return fmt.Errorf("exporter.fluent-forward-log-export[%d].name %q is not unique", i, exporter.Name)
 		}
 		exporterNames[exporter.Name] = struct{}{}
 	}
@@ -44,6 +58,34 @@ func HasMetricsExporter(name string, e *Exporters) bool {
 		return false
 	}
 	for _, exporter := range e.OtlpMetricExport {
+		if exporter.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func GetLogsExporter(name string, e *Exporters) (types.Sink, error) {
+	if e == nil {
+		return nil, fmt.Errorf("exporters config not set")
+	}
+	for _, exporter := range e.FluentForwardLogExport {
+		if exporter.Name == name {
+			opts := export.LogToFluentExporterOpts{
+				Destination:  exporter.Destination,
+				TagAttribute: exporter.TagAttribute,
+			}
+			return export.NewLogToFluentExporter(opts)
+		}
+	}
+	return nil, fmt.Errorf("exporter %s not found", name)
+}
+
+func HasLogsExporter(name string, e *Exporters) bool {
+	if e == nil {
+		return false
+	}
+	for _, exporter := range e.FluentForwardLogExport {
 		if exporter.Name == name {
 			return true
 		}
@@ -175,4 +217,47 @@ func getLabelMappings(mappings []LabelMatcher) (map[*regexp.Regexp]*regexp.Regex
 		regexMappings[kRegex] = vRegex
 	}
 	return regexMappings, nil
+}
+
+type FluentForwardLogExport struct {
+	Name         string `toml:"name" comment:"Name of the exporter."`
+	Destination  string `toml:"destination" comment:"Fluentd/Fluent Bit endpoint to send logs to. Must be in the form tcp://<host>:<port> or unix:///path/to/socket."`
+	TagAttribute string `toml:"tag-attribute" comment:"Attribute key to use as the tag for the log. If the attribute is not set, the log will be ignored by this exporter."`
+}
+
+func (f *FluentForwardLogExport) Validate() error {
+	if f.Name == "" {
+		return fmt.Errorf("name must be set")
+	}
+	if f.Destination == "" {
+		return fmt.Errorf("destination must be set")
+	}
+
+	if strings.HasPrefix(f.Destination, "unix://") {
+		socketPath := strings.TrimPrefix(f.Destination, "unix://")
+		if len(socketPath) == 0 {
+			return fmt.Errorf("invalid destination %s: unix socket path is empty", f.Destination)
+		}
+	} else if strings.HasPrefix(f.Destination, "tcp://") {
+		destinationHostPort := strings.TrimPrefix(f.Destination, "tcp://")
+		_, port, err := net.SplitHostPort(destinationHostPort)
+		if err != nil {
+			return fmt.Errorf("invalid destination %s: %w", f.Destination, err)
+		}
+
+		numericPort, err := strconv.ParseInt(port, 10, 0)
+		if err != nil {
+			return fmt.Errorf("invalid destination %s: unable to parse %s as integer", f.Destination, port)
+		}
+		if numericPort < 1 || numericPort > 65535 {
+			return fmt.Errorf("invalid destination %s: port %d not between 1 and 65535", f.Destination, numericPort)
+		}
+	} else {
+		return fmt.Errorf("invalid destination %s: must be in the form tcp://<host>:<port> or unix:///path/to/socket", f.Destination)
+	}
+
+	if f.TagAttribute == "" {
+		return fmt.Errorf("tag-attribute must be set")
+	}
+	return nil
 }
--- cmd/collector/config/export_test.go
+++ cmd/collector/config/export_test.go
@@ -23,6 +23,13 @@ func TestExporters_Validate(t *testing.T) {
 						Destination: "https://localhost:9393",
 					},
 				},
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "fluent-forward-name",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
 			},
 			expectedErr: "",
 		},
@@ -41,6 +48,44 @@ func TestExporters_Validate(t *testing.T) {
 				},
 			},
 			expectedErr: "exporter.otlp-metric-export[1].name must be set",
+		},
+		{
+			name: "duplicate name for exporter",
+			input: Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "name",
+						Destination: "https://localhost:9393",
+					},
+				},
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "name",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			expectedErr: "exporter.fluent-forward-log-export[0].name \"name\" is not unique",
+		},
+		{
+			name: "invalid missing name for fluent exporter",
+			input: Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "name",
+						Destination: "https://localhost:9393",
+					},
+				},
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			expectedErr: "exporter.fluent-forward-log-export[0].name must be set",
 		},
 	}
 
@@ -155,6 +200,360 @@ func TestOtlpMetricExport_Validate(t *testing.T) {
 				require.Error(t, err)
 				require.Equal(t, c.expectedErr, err.Error())
 			}
+		})
+	}
+}
+
+func TestFluentForwardLogExport_Validate(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       FluentForwardLogExport
+		expectedErr string
+	}{
+		{
+			name: "valid config",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://localhost:24224",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "",
+		},
+		{
+			name: "missing name",
+			input: FluentForwardLogExport{
+				Destination:  "tcp://localhost:24224",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "name must be set",
+		},
+		{
+			name: "missing destination",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "destination must be set",
+		},
+		{
+			name: "missing tag attribute",
+			input: FluentForwardLogExport{
+				Name:        "test-exporter",
+				Destination: "tcp://localhost:24224",
+			},
+			expectedErr: "tag-attribute must be set",
+		},
+		{
+			name: "unix socket destination",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "unix:///var/run/fluent.sock",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "",
+		},
+		{
+			name: "no host",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://:24224",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "",
+		},
+		{
+			name: "invalid tcp destination - no port",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://localhost",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination tcp://localhost: address localhost: missing port in address",
+		},
+		{
+			name: "invalid tcp destination - invalid port",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://localhost:invalid",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination tcp://localhost:invalid: unable to parse invalid as integer",
+		},
+		{
+			name: "invalid tcp destination - too low of port",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://localhost:0",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination tcp://localhost:0: port 0 not between 1 and 65535",
+		},
+		{
+			name: "invalid tcp destination - too high of port",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "tcp://localhost:65536",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination tcp://localhost:65536: port 65536 not between 1 and 65535",
+		},
+		{
+			name: "invalid destination - wrong prefix",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "file:///var/run/fluent.sock",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination file:///var/run/fluent.sock: must be in the form tcp://<host>:<port> or unix:///path/to/socket",
+		},
+		{
+			name: "invalid destination - no prefix",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "localhost",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination localhost: must be in the form tcp://<host>:<port> or unix:///path/to/socket",
+		},
+		{
+			name: "invalid unix socket - missing path",
+			input: FluentForwardLogExport{
+				Name:         "test-exporter",
+				Destination:  "unix://",
+				TagAttribute: "log_tag",
+			},
+			expectedErr: "invalid destination unix://: unix socket path is empty",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+			if tt.expectedErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tt.expectedErr, err.Error())
+			}
+		})
+	}
+}
+
+func TestGetMetricsExporter(t *testing.T) {
+	tests := []struct {
+		name         string
+		exporters    *Exporters
+		exporterName string
+		wantErr      bool
+		errMsg       string
+	}{
+		{
+			name: "existing exporter",
+			exporters: &Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "test-exporter",
+						Destination: "https://localhost:9393",
+					},
+				},
+			},
+			exporterName: "test-exporter",
+			wantErr:      false,
+		},
+		{
+			name:         "nil exporters",
+			exporters:    nil,
+			exporterName: "test-exporter",
+			wantErr:      true,
+			errMsg:       "exporters config not set",
+		},
+		{
+			name: "non-existing exporter",
+			exporters: &Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "test-exporter",
+						Destination: "https://localhost:9393",
+					},
+				},
+			},
+			exporterName: "non-existing",
+			wantErr:      true,
+			errMsg:       "exporter non-existing not found",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := GetMetricsExporter(tt.exporterName, tt.exporters)
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Equal(t, tt.errMsg, err.Error())
+				require.Nil(t, client)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, client)
+			}
+		})
+	}
+}
+
+func TestHasMetricsExporter(t *testing.T) {
+	tests := []struct {
+		name         string
+		exporters    *Exporters
+		exporterName string
+		want         bool
+	}{
+		{
+			name: "existing exporter",
+			exporters: &Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "test-exporter",
+						Destination: "https://localhost:9393",
+					},
+				},
+			},
+			exporterName: "test-exporter",
+			want:         true,
+		},
+		{
+			name:         "nil exporters",
+			exporters:    nil,
+			exporterName: "test-exporter",
+			want:         false,
+		},
+		{
+			name: "non-existing exporter",
+			exporters: &Exporters{
+				OtlpMetricExport: []*OtlpMetricExport{
+					{
+						Name:        "test-exporter",
+						Destination: "https://localhost:9393",
+					},
+				},
+			},
+			exporterName: "non-existing",
+			want:         false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := HasMetricsExporter(tt.exporterName, tt.exporters)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestGetLogsExporter(t *testing.T) {
+	tests := []struct {
+		name         string
+		exporters    *Exporters
+		exporterName string
+		wantErr      bool
+		errMsg       string
+	}{
+		{
+			name: "existing exporter",
+			exporters: &Exporters{
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "test-exporter",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			exporterName: "test-exporter",
+			wantErr:      false,
+		},
+		{
+			name:         "nil exporters",
+			exporters:    nil,
+			exporterName: "test-exporter",
+			wantErr:      true,
+			errMsg:       "exporters config not set",
+		},
+		{
+			name: "non-existing exporter",
+			exporters: &Exporters{
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "test-exporter",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			exporterName: "non-existing",
+			wantErr:      true,
+			errMsg:       "exporter non-existing not found",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sink, err := GetLogsExporter(tt.exporterName, tt.exporters)
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Equal(t, tt.errMsg, err.Error())
+				require.Nil(t, sink)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, sink)
+			}
+		})
+	}
+}
+
+func TestHasLogsExporter(t *testing.T) {
+	tests := []struct {
+		name         string
+		exporters    *Exporters
+		exporterName string
+		want         bool
+	}{
+		{
+			name: "existing exporter",
+			exporters: &Exporters{
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "test-exporter",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			exporterName: "test-exporter",
+			want:         true,
+		},
+		{
+			name:         "nil exporters",
+			exporters:    nil,
+			exporterName: "test-exporter",
+			want:         false,
+		},
+		{
+			name: "non-existing exporter",
+			exporters: &Exporters{
+				FluentForwardLogExport: []*FluentForwardLogExport{
+					{
+						Name:         "test-exporter",
+						Destination:  "tcp://localhost:24224",
+						TagAttribute: "log_tag",
+					},
+				},
+			},
+			exporterName: "non-existing",
+			want:         false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := HasLogsExporter(tt.exporterName, tt.exporters)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
