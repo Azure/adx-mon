890d69d20220648061ac20dc0e28849262c22cd1
Mike Keesey <mikekeesey@microsoft.com>
2025-04-25 19:43:27 +0000

    Configure otlp logs in main along with other collectors
    We now configure these in a consistent place to have access to config.

--- collector/log_collection.go
+++ collector/log_collection.go
@@ -2,11 +2,21 @@ package collector
 
 import (
 	"github.com/Azure/adx-mon/collector/logs"
+	"github.com/Azure/adx-mon/ingestor/cluster"
+	"github.com/Azure/adx-mon/pkg/http"
 	"github.com/Azure/adx-mon/storage"
 )
 
+// LogCollectorOpts is the options for creating a log collector.
 type LogCollectorOpts struct {
 	Create createFunc
 }
 
+// HttpLogCollectorOpts is the options for creating a log collector with an HTTP endpoint.
+type HttpLogCollectorOpts struct {
+	CreateHTTPSvc createHttpFunc
+}
+
 type createFunc func(store storage.Store) (*logs.Service, error)
+
+type createHttpFunc func(store storage.Store, health *cluster.Health) (*logs.Service, *http.HttpHandler, error)
