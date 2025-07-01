ef756ffe03e5569eb9612426c7636ece4cf25a69
Jason Wilder <jawilder@microsoft.com>
2025-07-01 13:25:28 -0600

    Remove metric dimension from segment size metric
    We typically just sum all the metrics to see how much disk usage
    each node is using.  Since we already have that data, it's cheaper
    to just record it directly.

--- metrics/service.go
+++ metrics/service.go
@@ -132,7 +132,6 @@ func (s *service) collect(ctx context.Context) {
 							}
 						}
 					}
-
 				}
 			}
 
@@ -188,7 +187,6 @@ func (s *service) collect(ctx context.Context) {
 
 			// Clear the gauges to prune old metrics that may not be collected anymore.
 			IngestorSegmentsMaxAge.Reset()
-			IngestorSegmentsSizeBytes.Reset()
 			IngestorSegmentsTotal.Reset()
 			LogsProxyUploaded.Reset()
 		}
